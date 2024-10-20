2164c5a1434ae770fb8190cdb404c152c08cda4c
Kariman <kariman.hesham@advaeg.com>
2024-10-20 15:03:57 +0300

    add framework

---
+++ MyFramework.xcframework/ios-arm64_x86_64-simulator/AdvaFrameWork.framework/Headers/AdvaFrameWork.h
@@ -0,0 +1,18 @@
+//
+//  AdvaFrameWork.h
+//  AdvaFrameWork
+//
+//  Created by Eslam on 17/09/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AdvaFrameWork.
+FOUNDATION_EXPORT double AdvaFrameWorkVersionNumber;
+
+//! Project version string for AdvaFrameWork.
+FOUNDATION_EXPORT const unsigned char AdvaFrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AdvaFrameWork/PublicHeader.h>
+
+
